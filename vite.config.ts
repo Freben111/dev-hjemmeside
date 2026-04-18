d0f9f2a4ac7f9a7a3835dba0af3f269c926e3893
Freben111 <frederikbengtson0@gmail.com>
2026-04-18 13:27:09 +0200

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
